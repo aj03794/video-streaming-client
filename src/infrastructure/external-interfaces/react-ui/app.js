60238965732405384c71e44065fc9c3015cb52e1
axj1l59 <adam_johnston1@homedepot.com>
2019-03-24 12:17:53 -0400

    Setting up project to have a react ui

---
+++ src/infrastructure/external-interfaces/react-ui/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './themes/main.scss'
+
+export class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,59 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtract = require('mini-css-extract-plugin')
+const webpackMd5 = require('webpack-md5-hash')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
+
+const isDev = process.env.ENV === 'dev'
+const styleLoader = isDev ? 'style-loader' : miniCssExtract.loader
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-      contentBase: './dist'
-  }
-};
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    styleLoader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    devServer: {
+        contentBase: path.join(__dirname, "dist/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new htmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/infrastructure/external-interfaces/react-ui/index.html',
+            filename: 'index.html',
+            title: 'Coopie Cam'
+        }),
+        new miniCssExtract({
+            filename: isDev ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDev ? '[id].css' : '[id].[hash].css'
+        }),
+        new webpackMd5(),
+        new cleanWebpackPlugin()
+    ]
+}
